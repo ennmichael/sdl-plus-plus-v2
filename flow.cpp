37da50734bf767e3388142fadbeee2d8ad6875d4
ennmichael <enntheprogrammer@gmail.com>
2017-01-12 15:06:30 +0100

    Formated, added some multithreading support

--- flow.cpp
+++ flow.cpp
@@ -1,66 +1,84 @@
 #include "flow.h"
 #include "SDL2/SDL.h"
 
-namespace Sdl
-{
-    Basic_loop::Basic_loop(Function f) noexcept
-        : m_f { f }
-    {
+namespace Sdl {
+bool key_down(SDL_Scancode scancode) {
+    return SDL_GetKeyboardState(nullptr)[scancode];
+}
+
+Basic_loop::Basic_loop(Function f) noexcept : m_f{f} {}
+
+void Basic_loop::start() {
+    while (!SDL_QuitRequested()) {
+        m_f();
     }
-    
-    void Basic_loop::start()
-    {
-        while (!SDL_QuitRequested())
-        {
-            m_f();
+}
+
+Event_loop::Event_loop(Event_function event_f,
+                       Unconditional_function unconditional_f) noexcept
+    : m_event_f{event_f},
+      m_unconditional_f{unconditional_f} {}
+
+void Event_loop::start() {
+    while (true) {
+        while (SDL_PollEvent(&m_event)) {
+            if (m_event_f(m_event) == Event_result::Quit)
+                return;  // Quit if the user says so
         }
+
+        m_unconditional_f();
     }
-    
-    Event_loop::Event_loop(Event_function event_f, 
-                           Unconditional_function unconditional_f) noexcept
-        : m_event_f { event_f }
-        , m_unconditional_f { unconditional_f }
-    { }
-    
-    void Event_loop::start()
-    {
-        while (true)
-        {
-            while (SDL_PollEvent(&m_event))
-            {
-                if (m_event_f(m_event) == Event_result::Quit)
-                    return; // Quit if the user says so
+}
+
+void Thread_manager::add_graphics_thread(Graphics_init_function init,
+                                         Graphics_thread_function f) {
+    threads_.push_back(std::thread{[this, init, f] {
+        auto screen = init();
+
+        while (!quit_) {
+            SDL_PumpEvents();
+            f(screen);
+            if (SDL_QuitRequested()) {
+                quit_ = true;
             }
-            
-            m_unconditional_f();
         }
-    }
-    
-    Timer::Timer(std::chrono::milliseconds delay) noexcept
-    {
-        set_delay(delay);
-    }
-    
-    bool Timer::ready() noexcept
-    {
-        auto ticks = SDL_GetTicks();
-        
-        if (ticks >= delay_ + current_)
-        {
-            current_ = ticks;
-            return true;
+    }});
+}
+
+void Thread_manager::add_thread(Thread_function f) {
+    threads_.push_back(std::thread{[f, this] {
+        while (!quit_) {
+            f();
         }
-        
-        return false;
+    }});
+}
+
+void Thread_manager::start_all() {
+    for (auto& thread : threads_) {
+        thread.join();
     }
-    
-    std::chrono::milliseconds Timer::get_delay() const noexcept
-    {
-        return std::chrono::milliseconds { delay_ };
+}
+
+Timer::Timer(std::chrono::milliseconds delay) noexcept {
+    set_delay(delay);
+}
+
+bool Timer::ready() noexcept {
+    auto ticks = SDL_GetTicks();
+
+    if (ticks >= delay_ + current_) {
+        current_ = ticks;
+        return true;
     }
-    
-    void Timer::set_delay(std::chrono::milliseconds delay) noexcept
-    {
-        delay_ = static_cast <Uint32>(delay.count());
-    }
+
+    return false;
+}
+
+std::chrono::milliseconds Timer::get_delay() const noexcept {
+    return std::chrono::milliseconds{delay_};
+}
+
+void Timer::set_delay(std::chrono::milliseconds delay) noexcept {
+    delay_ = static_cast<Uint32>(delay.count());
+}
 }
--- flow.h
+++ flow.h
@@ -5,59 +5,79 @@
 #include <functional>
 #include <chrono>
 #include <stdexcept>
+#include <thread>
+#include <atomic>
 
-namespace Sdl
-{
-    class Basic_loop {
-    public:
-        using Function = std::function<void(void)>;
-        
-        explicit Basic_loop(Function) noexcept;
-        void start();
-        
-    private:
-        Function m_f { };
-    };
-    
-    enum class Event_result : bool {
-        Quit, Continue
-    };
-    
-    class Event_loop {
-    public:
-        using Event_function = std::function<Event_result(const SDL_Event&)>;
-        using Unconditional_function = std::function<void(void)>;
-        
-        Event_loop(Event_function, Unconditional_function) noexcept;
-        void start();
-        
-    private:
-        Event_function m_event_f { };
-        Unconditional_function m_unconditional_f { };
-        SDL_Event m_event { };
-    };
-    
-    // These loops can share a common concept in c++17
-    
-    // TODO maybe an option to change the timers delay?
-    class Timer {
-    public:
-        explicit Timer(std::chrono::milliseconds) noexcept;
-        /*
-         * The user is expected to give a sane value, larger than 0,
-         * not overflowing Uint32. Because small values are pretty much expected,
-         * none of this is actually checked, so beware or check it yourself.
-         */
-        
-        bool ready() noexcept;
-        
-        std::chrono::milliseconds get_delay() const noexcept;
-        void set_delay(std::chrono::milliseconds) noexcept;
-        
-    private:
-        Uint32 delay_ { };
-        Uint32 current_ { SDL_GetTicks() };
-    };
+namespace Sdl {
+bool key_down(SDL_Scancode);
+
+class Basic_loop {
+   public:
+    using Function = std::function<void(void)>;
+
+    explicit Basic_loop(Function) noexcept;
+    void start();
+
+   private:
+    Function m_f{};
+};
+
+enum class Event_result : bool { Quit, Continue };
+
+// Maybe add a Pump_event_loop class?
+
+class Event_loop {
+   public:
+    using Event_function = std::function<Event_result(const SDL_Event&)>;
+    using Unconditional_function = std::function<void(void)>;
+
+    Event_loop(Event_function, Unconditional_function) noexcept;
+    void start();
+
+   private:
+    Event_function m_event_f{};
+    Unconditional_function m_unconditional_f{};
+    SDL_Event m_event{};
+};
+
+class Thread_manager {
+   public:
+    using Quit_flag = std::atomic<bool>;
+    using Thread_function = std::function<void()>;
+    using Graphics_init_function = std::function<Screen()>;
+    using Graphics_thread_function = std::function<void(Screen&)>;
+
+    void add_graphics_thread(Graphics_init_function,
+                             Graphics_thread_function f);
+    void add_thread(Thread_function f);
+    void start_all();
+
+   private:
+    Quit_flag quit_{false};
+    std::vector<std::thread> threads_{};
+};
+
+// These loops can share a common concept in c++17
+
+// TODO maybe an option to change the timers delay?
+class Timer {
+   public:
+    explicit Timer(std::chrono::milliseconds) noexcept;
+    /*
+     * The user is expected to give a sane value, larger than 0,
+     * not overflowing Uint32. Because small values are pretty much expected,
+     * none of this is actually checked, so beware or check it yourself.
+     */
+
+    bool ready() noexcept;
+
+    std::chrono::milliseconds get_delay() const noexcept;
+    void set_delay(std::chrono::milliseconds) noexcept;
+
+   private:
+    Uint32 delay_{};
+    Uint32 current_{SDL_GetTicks()};
+};
 }
 
 #endif
